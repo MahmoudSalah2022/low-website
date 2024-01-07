7042c5a3229dbe8843b12a7f707fcdc918c6702a
DESKTOP-JOB5LDJ\pc <mahmoudeahmed.salah@gmail.com>
2024-01-07 21:56:07 +0200

    add more

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+// const lawSection = document.querySelector('.lawSection');
+
+// lawSection.addEventListener('dragstart', dragStart);
+// lawSection.addEventListener('dragover', dragOver);
+// lawSection.addEventListener('drop', drop);
+
+// function dragStart(event) {
+//   event.dataTransfer.setData('text/plain', event.target.id);
+// }
+
+// function dragOver(event) {
+//   event.preventDefault();
+// }
+
+// function drop(event) {
+//   event.preventDefault();
+//   const data = event.dataTransfer.getData('text/plain');
+//   const draggableElement = document.getElementById(data);
+//   const dropzone = event.target;
+//   dropzone.appendChild(draggableElement);
+// }
     
