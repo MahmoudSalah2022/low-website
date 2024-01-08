2aa617f5bee5be7050abca550e3afa0f52f5ca86
DESKTOP-JOB5LDJ\pc <mahmoudeahmed.salah@gmail.com>
2024-01-08 14:02:10 +0200

    closebutton

---
+++ js/orderConsultant.js
@@ -0,0 +1,33 @@
+const closeBtn = document.querySelectorAll('.closeBtn')
+const payedConsultant = document.querySelector('.payedConsultantConatainer')
+
+closeBtn.forEach(close => {
+
+    close.addEventListener('click', () => {
+        if(payedConsultant.classList.contains('display')){
+            payedConsultant.classList.remove('display')
+            payedConsultant.classList.add('hidden')
+        }else{
+            payedConsultant.classList.add('display')
+            payedConsultant.classList.remove('hidden')
+        }
+    });
+    
+});
+
+
+const orderButton = document.querySelectorAll('.orderButton')
+const orderPayedConsultant = document.querySelector('.payedConsultantConatainer')
+orderButton.forEach(order => {
+
+    order.addEventListener('click', () => {
+        if(orderPayedConsultant.classList.contains('display')){
+            orderPayedConsultant.classList.remove('display')
+            orderPayedConsultant.classList.add('hidden')
+        }else{
+            orderPayedConsultant.classList.add('display')
+            orderPayedConsultant.classList.remove('hidden')
+        }
+    });
+    
+});
