6e71648e337a48bd713e875be625578e7269a143
DESKTOP-JOB5LDJ\pc <mahmoudeahmed.salah@gmail.com>
2024-01-09 13:28:14 +0200

    edit in video and header

--- js/storyvideoLower.js
+++ js/storyvideoLower.js
@@ -3,6 +3,7 @@ const videoStoryContainer = document.querySelector('.videoStoryContainer')
 closeBtn.forEach(close => {
 
     close.addEventListener('click', () => {
+        document.querySelector('body').classList.toggle('no-scroll');
         if(videoStoryContainer.classList.contains('display')){
             videoStoryContainer.classList.remove('display')
             videoStoryContainer.classList.add('hidden')
@@ -17,6 +18,7 @@ const storyAccount = document.querySelectorAll('.storyAccount')
 storyAccount.forEach(story => {
 
     story.addEventListener('click', () => {
+        document.querySelector('body').classList.toggle('no-scroll');
         if(videoStoryContainer.classList.contains('display')){
             videoStoryContainer.classList.remove('display')
             videoStoryContainer.classList.add('hidden')
